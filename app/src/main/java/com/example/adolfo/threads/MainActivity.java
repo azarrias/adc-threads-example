c94deff26b1a9adabe80d1d0a8cbb4d65b8d6952
azarrias <adolfo.zarrias@gmail.com>
2019-04-14 10:34:59 +0200

    Add ProgressDialog to AsyncTask

--- app/src/main/java/com/example/adolfo/threads/MainActivity.java
+++ app/src/main/java/com/example/adolfo/threads/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.adolfo.threads;
 
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
@@ -33,20 +34,43 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    class MyTask extends AsyncTask<Integer, Void, Integer> {
-        private int res;
+    class MyTask extends AsyncTask<Integer, Integer, Integer> {
+        private ProgressDialog progress;
+
+        @Override
+        protected void onPreExecute() {
+            progress = new ProgressDialog(MainActivity.this);
+            progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            progress.setMessage("Calculating...");
+            progress.setCancelable(false);
+            progress.setMax(100);
+            progress.setProgress(0);
+            progress.show();
+        }
 
         @Override
         protected Integer doInBackground(Integer... integers) {
             // Executes on a secondary thread
-            res = factorial(integers[0]);
+            int res = 1;
+            for (int i=1; i <= integers[0]; i++) {
+                res *= i;
+                SystemClock.sleep(1000);
+                publishProgress(i * 100 / integers[0]);
+            }
+            //res = factorial(integers[0]);
             return res;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progress.setProgress(values[0]);
         }
 
         @Override
         protected void onPostExecute(Integer integer) {
             // Executes on the UI thread
-            tvOutput.append(res + "\n");
+            progress.dismiss();
+            tvOutput.append(integer + "\n");
         }
     }
 
